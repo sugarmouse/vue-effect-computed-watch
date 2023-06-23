e106bef79d229209cee30e4ac3ff019227a37479
sugarmouse <tanghao19950911@gmail.com>
2023-06-24 04:34:23 +0800

    add tokenize function

--- src/compiler.ts
+++ src/compiler.ts
@@ -3,6 +3,112 @@ type AST = object;
 type JSAST = object;
 type RenderFuntion = (...arg: any[]) => any;
 
+enum State {
+    Initial,
+    TagOpen,
+    TagName,
+    Text,
+    TagEnd,
+    TagEndName,
+}
+
+function isAlpha(char: string) {
+    return char >= 'a' && char <= 'z' || char >= 'A' && char <= 'Z';
+}
+
+function tokenize(str: Template): AST {
+    let currentState: State = State.Initial;
+    const chars: string[] = [];
+
+    const tokens: any[] = [];
+
+    while (str.length !== 0) {
+        const char = str[0];
+
+        switch (currentState as State) {
+            case State.Initial:
+                if (char === '<') {
+                    currentState = State.TagOpen;
+                    str = str.slice(1);
+                } else if (isAlpha(char)) {
+                    currentState = State.Text;
+                    chars.push(char);
+                    str = str.slice(1);
+                }
+                break;
+
+            case State.TagOpen:
+                if (isAlpha(char)) {
+                    currentState = State.TagName;
+                    chars.push(char);
+                    str = str.slice(1);
+                } else if (char === '/') {
+                    currentState = State.TagEnd;
+                    str = str.slice(1);
+                }
+                break;
+
+            case State.TagName:
+                if (isAlpha(char)) {
+                    chars.push(char);
+                    str = str.slice(1);
+                } else if (char === '>') {
+                    currentState = State.Initial;
+
+                    tokens.push({
+                        type: 'tag',
+                        value: chars.join('')
+                    });
+                    chars.length = 0;
+                    str = str.slice(1);
+                }
+                break;
+
+            case State.Text:
+                if (isAlpha(char)) {
+                    chars.push(char);
+                    str = str.slice(1);
+                } else if (char === '<') {
+                    currentState = State.TagOpen;
+
+                    tokens.push({
+                        type: 'text',
+                        content: chars.join('')
+                    });
+                    chars.length = 0;
+                    str = str.slice(1);
+                }
+                break;
+
+            case State.TagEnd:
+                if (isAlpha(char)) {
+                    currentState = State.TagEndName;
+                    chars.push(char);
+                    str = str.slice(1);
+                }
+                break;
+
+            case State.TagEndName:
+                if (isAlpha(char)) {
+                    chars.push(char);
+                    str = str.slice(1);
+                } else if (char === '>') {
+                    currentState = State.Initial;
+                    tokens.push({
+                        type: 'tagEnd',
+                        value: chars.join('')
+                    });
+
+                    chars.length = 0;
+                    str = str.slice(1);
+
+                }
+                break;
+        }
+    }
+    return tokens;
+}
+
 // template code -> template AST
 function parse(template: Template): AST {
 
